a00073970a93328ccce43062e8aeec4fca00bae8
Baghdad <baghdadalkhatib@gmail.com>
2018-04-13 18:37:15 +0300

    my first commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "MyGitTest"
+
+version := "0.1"
+
+scalaVersion := "2.12.5"
---
+++ src/main/scala/GitTest.scala
@@ -0,0 +1,5 @@
+object GitTest {
+  def main(args: Array[String]): Unit = {
+    println("hello world !")
+  }
+}
